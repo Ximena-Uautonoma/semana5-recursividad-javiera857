14ed08bd79844ddbd80f821f537c5f2b728ae95f
javiera857 <javiera.trujillo2@cloud.uautonoma.cl>
2026-03-30 18:21:39 +0000

    yhyhyh

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -7,32 +7,13 @@ Debe implementar dos funciones:
 """
 
 def contar_ciclo(n):
-<<<<<<< HEAD
-=======
-    listaNumeros = []
-    for i in range (1, n+1):
-        listaNumeros.append(i)
-    return listaNumeros
-print(contar_ciclo(10))
->>>>>>> e7d105cea1e2781831cd73c651db2ca6e31939f6
 
 listaNumeros = []
 for i in range (1, n+1):
     listaNumeros.append(i):
     return listaNumeros
 
-<<<<<<< HEAD
 print(contar_ciclo(8))
-=======
-def contar_recursivo(n):
-  if n < 1:
-      return []
-else:
->>>>>>> e7d105cea1e2781831cd73c651db2ca6e31939f6
 
-<<<<<<< HEAD
 def contar_recursivo(n):
    
-=======
-
->>>>>>> e7d105cea1e2781831cd73c651db2ca6e31939f6
